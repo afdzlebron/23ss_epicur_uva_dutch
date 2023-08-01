e5e2e6ed459e738bcf9965a1518d847bb4ac695c
afdzlebron <afdz.lebron@gmail.com>
2023-08-01 14:05:55 +0200

    commit

--- app.js
+++ app.js
@@ -1,83 +1,80 @@
-Here’s a possible implementation of the learn.html template and the corresponding JavaScript code for loading a random word from the JSON file, displaying the collocations, checking the answer, and displaying feedback icons:
-
-<!-- Define the learn page template -->
-<template id="learn.html">
-    <ons-page>
-        <!-- Use ons-toolbar to display a header with a back button -->
-        <ons-toolbar>
-            <div class="left">
-                <ons-back-button>Back</ons-back-button>
-            </div>
-            <div class="center">Learn</div>
-        </ons-toolbar>
-
-        <!-- Use ons-card to display a word and its collocations -->
-        <ons-card id="word-card">
-            <!-- Use JavaScript to load a random word from the JSON file -->
-            <h2 id="word"></h2>
-            <div id="collocations"></div>
-        </ons-card>
-
-        <!-- Use ons-fab to display a button to go to the next word -->
-        <ons-fab position="bottom right" onclick="nextWord()">
-            <ons-icon icon="md-arrow-forward"></ons-icon>
-        </ons-fab>
-    </ons-page>
-</template>
-Kopieren
 // Define global variables
 let words = [];
 let currentWordIndex = 0;
 
-// Load words from the JSON file
-function loadWords() {
-    fetch('words.json')
-        .then(response => response.json())
-        .then(data => {
-            words = data.words;
-            nextWord();
-        });
-}
+// Define utility functions
+const fn = {};
 
-// Load the next word
-function nextWord() {
-    // Get the current word and its collocations
-    const word = words[currentWordIndex];
-    const collocations = word.collocations;
+// Load a page into the navigator
+fn.load = function (page) {
+  const navigator = document.querySelector("#navigator");
+  navigator.bringPageTop(page);
+};
 
-    // Update the word card with the new word and its collocations
-    const wordElement = document.querySelector('#word');
-    const collocationsElement = document.querySelector('#collocations');
-    wordElement.textContent = word.word;
-    collocationsElement.innerHTML = '';
-    for (const collocation of collocations) {
-        const button = document.createElement('ons-button');
-        button.textContent = collocation;
-        button.onclick = () => checkAnswer(collocation);
-        collocationsElement.appendChild(button);
-    }
+// Load the next word in the learn page
+fn.nextWord = function () {
+  // Get the current word and its collocations
+  const word = words[currentWordIndex];
+  const collocations = word.collocations;
 
-    // Increment the current word index
-    currentWordIndex = (currentWordIndex + 1) % words.length;
-}
+  // Update the word card with the new word and its collocations
+  const wordElement = document.querySelector("#word");
+  const collocationsElement = document.querySelector("#collocations");
+  wordElement.textContent = word.word;
+  collocationsElement.innerHTML = "";
+  for (const collocation of collocations) {
+    const button = document.createElement("ons-button");
+    button.textContent = collocation;
+    button.onclick = () => fn.checkAnswer(collocation);
+    collocationsElement.appendChild(button);
+  }
+
+  // Increment the current word index
+  currentWordIndex = (currentWordIndex + 1) % words.length;
+};
 
 // Check the user's answer
-function checkAnswer(collocation) {
-    // Get the current word and its correct collocation
-    const word = words[currentWordIndex - 1];
-    const correctCollocation = word.collocation;
+fn.checkAnswer = function (collocation) {
+  // Get the current word and its correct collocation
+  const word = words[currentWordIndex - 1];
+  const correctCollocation = word.collocation;
 
-    // Check if the user's answer is correct
-    if (collocation === correctCollocation) {
-        // Display a success icon
-        ons.notification.toast('Correct!', {buttonLabel: 'OK', timeout: 2000});
-    } else {
-        // Display an error icon
-        ons.notification.toast('Incorrect!', {buttonLabel: 'OK', timeout: 2000});
-    }
-}
+  // Check if the user's answer is correct
+  if (collocation === correctCollocation) {
+    // Display a success icon
+    ons.notification.toast("Correct!", { buttonLabel: "OK", timeout: 2000 });
+  } else {
+    // Display an error icon
+    ons.notification.toast("Incorrect!", { buttonLabel: "OK", timeout: 2000 });
+  }
+};
+
+// Load words from the JSON file
+fn.loadWords = function () {
+  // Use fetch to load the JSON file
+  fetch("words.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Store the words in the global variable
+      words = data.words;
+      fn.nextWord();
+    });
+};
+
+// Save changes to GitHub
+fn.saveChanges = function () {
+  // Collect user input for GitHub account and password
+  const emailInput = document.querySelector("#email-input");
+  const passwordInput = document.querySelector("#password-input");
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  // Use GitHub API to save changes
+  // ...
+};
 
 // Initialize the app
-window.addEventListener('load', () => {
-    loadWords();
+ons.ready(function () {
+  // Load words from the JSON file
+  fn.loadWords();
 });
